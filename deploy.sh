e74a1acc62369edf826f9a7939d2aba13880c039
moose <moosecodes@gmail.com>
2023-04-17 18:31:55 -0700

    update deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "---> ROOT CONFIG" && echo "" && cd /var/www/mc-spa-root-config && pwd
+git pull && npm run build && mv ./dist/* ../html && cp ./importmap.json ../html && cp ./deploy.sh /var/www
+echo "---> STYLEGUIDE" && echo "" && cd /var/www/html/mc-styleguide && pwd && git pull && npm run build
+echo "---> NEWS UI" && echo "" && cd /var/www/html/mc-news-ui && pwd && git pull && npm run build
+echo "---> PRIMARY NAV UI" && echo "" && cd /var/www/html/mc-primary-nav-ui && pwd && git pull && npm run build
+echo "--> MOVIES UI" && echo "" && cd /var/www/html/mc-movies-ui && pwd && git pull && npm run build
