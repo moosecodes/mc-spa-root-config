ad73a678a4a8bc7a4ec2cde881123bcc7d65d69d
moose <moosecodes@gmail.com>
2023-04-17 18:13:31 -0700

    fix missing styleguide

--- src/moosecodes-root-config.js
+++ src/moosecodes-root-config.js
@@ -5,6 +5,7 @@ import {
   constructLayoutEngine,
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
+import "@moosecodes/mc-styleguide";
 
 const routes = constructRoutes(microfrontendLayout);
 const applications = constructApplications({
